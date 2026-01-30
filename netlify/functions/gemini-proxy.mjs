6073ec735a78b7849a1c2fa15aeb11cbdde4516b
Abigail <abigailf03@hotmail.ca>
2026-01-30 13:25:11 -0600

    fix api url

--- netlify/functions/gemini-proxy.mjs
+++ netlify/functions/gemini-proxy.mjs
@@ -1,7 +1,7 @@
 export default async (request, context) => {
   // This uses the "GEMINI_API_KEY" from your Netlify dashboard
   const API_KEY = process.env.GEMINI_API_KEY;
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`;
 
   try {
     const body = await request.json();
